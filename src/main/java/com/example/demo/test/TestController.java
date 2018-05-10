c5cf784b507bf2f97dcbba9563cef773a6d8d31c
qian <fengqian2018@dingtalk.com>
2018-05-10 10:07:17 +0800

    参数传递

--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -1,9 +1,6 @@
 package com.example.demo.test;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author fengqian
@@ -12,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping(value = "/getTest/{name}",method = RequestMethod.GET)
-    public String getTest(@PathVariable String name){
+    @RequestMapping(value = "/getTest/{name}", method = RequestMethod.GET)
+    public String getTest(@PathVariable String name) { //http://localhost:8081/getTest/feng
         System.err.println(name);
-        return name+"feng";
+        return name + "feng";
+    }
+
+    @RequestMapping(value = "/getTest1/", method = RequestMethod.GET)
+    public String getTest1(@RequestParam String name) { //http://localhost:8081/getTest1/?name=qian
+        System.err.println(name);
+        return name + "feng";
     }
 
 }
