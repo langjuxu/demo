481ce47f00e5cb77cf5a1cffaa5bccb7c40dacc3
qian <fengqian2018@dingtalk.com>
2018-05-16 16:12:33 +0800

    test list

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,20 +1,68 @@
+
 package com.example.demo;
 
+
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
 @RunWith(SpringRunner.class)
+
 @SpringBootTest
+
 public class DemoApplicationTests {
 
+
     @Test
+
     public void contextLoads() {
 
-        System.err.print("hahhaha");
+
+        List list = new ArrayList();
+
+        List list1 = new ArrayList();
+
+        List list2 = new ArrayList();
+
+        for (int i = 0; i < 10; i++) {
+
+            list1.add(i);
+
+            if (i % 2 == 0) {
+
+                list2.add(i);
+
+            }
+
+        }
+
+        System.err.println("list1:" + list1);
+
+        System.err.println("list2:" + list2);
+
+        list1.removeAll(list2);
+
+        list2.addAll(list1);
+
+        list.addAll(list2);
+
+        System.err.println("list:" + list);
+
     }
 
+
     public void mockTest() {
+
     }
+
 }
