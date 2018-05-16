bd8642343ef2990b4308a9aceba05a0c0da4b831
qian <fengqian2018@dingtalk.com>
2018-05-16 16:00:15 +0800

    Revert "test"
    This reverts commit 4ff4528

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,9 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -15,21 +12,7 @@ public class DemoApplicationTests {
     @Test
     public void contextLoads() {
 
-        List list = new ArrayList();
-        List list1 = new ArrayList();
-        List list2 = new ArrayList();
-        for (int i = 0; i < 10 ; i++){
-            list1.add(i);
-            if (i%2==0){
-                list2.add(i);
-            }
-        }
-        System.err.println("list1:"+list1);
-        System.err.println("list2:"+list2);
-        list1.removeAll(list2);
-        list2.addAll(list1);
-        list.addAll(list2);
-        System.err.println("list:"+list);
+        System.err.print("hahhaha");
     }
 
     public void mockTest() {
